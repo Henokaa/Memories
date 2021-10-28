3fdbf83936e08655c649b65841059812ed1c9df5
Henokaa <69524541+Henokaa@users.noreply.github.com>
2021-10-28 09:54:59 +0300

    configure imports

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,6 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import { getPosts } from './actions/posts';
+import useStyles from './styles';
+import memories from './images/memories.png';
 function App() {
   return (
     <div className="App">
