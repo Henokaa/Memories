9e4d05fe5b2d8f5958faa9132aaaebe533d22937
Henokaa <69524541+Henokaa@users.noreply.github.com>
2021-10-28 09:55:53 +0300

    building app.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -9,25 +9,11 @@ import Form from './components/Form/Form';
 import { getPosts } from './actions/posts';
 import useStyles from './styles';
 import memories from './images/memories.png';
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+  const classes = useStyles();
 
-export default App;
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
