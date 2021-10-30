d614073bbef38dab506de3cdc5a1933862561e82
Henokaa <69524541+Henokaa@users.noreply.github.com>
2021-10-30 10:05:10 +0300

    index.js work

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import { reducers } from './reducers';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
