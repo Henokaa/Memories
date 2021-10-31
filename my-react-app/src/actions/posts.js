7898f1410add0da8e9c8cd14af6bbd18ec02c8de
Henokaa <69524541+Henokaa@users.noreply.github.com>
2021-10-31 10:36:21 +0300

    posts js imports

---
+++ my-react-app/src/actions/posts.js
@@ -0,0 +1,9 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+
+import * as api from '../api/index.js';
+
+export const getPosts = () => async (dispatch) => {
+  try {
+    const { data } = await api.fetchPosts();
+  }
+}
