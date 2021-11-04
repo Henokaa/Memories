65ef00944c81fed438430c05b70dc4f574ec993f
Henokaa <69524541+Henokaa@users.noreply.github.com>
2021-11-04 14:46:20 +0300

    form js creation

--- my-react-app/src/components/Form/Form.js
+++ my-react-app/src/components/Form/Form.js
@@ -6,3 +6,11 @@ import FileBase from 'react-file-base64';
 import useStyles from './styles';
 import { createPost, updatePost } from '../../actions/posts';
 
+const Form = ({ currentId, setCurrentId }) => {
+  const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+  const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));
+  const dispatch = useDispatch();
+  const classes = useStyles();
+
+
+export default Form;
