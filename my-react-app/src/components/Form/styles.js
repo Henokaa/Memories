d389cdefb84d5ece29911c5a429f185bb4e9e5bd
Henokaa <69524541+Henokaa@users.noreply.github.com>
2021-11-04 14:50:01 +0300

    form styles

--- my-react-app/src/components/Form/styles.js
+++ my-react-app/src/components/Form/styles.js
@@ -7,5 +7,12 @@ export default makeStyles((theme) => ({
       margin: theme.spacing(1),
     },
   },
- 
+  paper: {
+    padding: theme.spacing(2),
+  },
+  form: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  
 }));
