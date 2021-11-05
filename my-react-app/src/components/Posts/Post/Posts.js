749ebe45291e06d9ac0a1aac62da91dfe00c33a5
Henokaa <69524541+Henokaa@users.noreply.github.com>
2021-11-05 23:47:30 +0300

    posts js

---
+++ my-react-app/src/components/Posts/Post/Posts.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
+import { useSelector } from 'react-redux';
+
+import Post from './Post/Post';
+import useStyles from './styles';
+
+
+export default Posts;
