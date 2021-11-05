f08a1896639c00714b5fe2a83567efda56053e46
Henokaa <69524541+Henokaa@users.noreply.github.com>
2021-11-05 23:47:59 +0300

    const posts js

--- my-react-app/src/components/Posts/Post/Posts.js
+++ my-react-app/src/components/Posts/Post/Posts.js
@@ -5,5 +5,13 @@ import { useSelector } from 'react-redux';
 import Post from './Post/Post';
 import useStyles from './styles';
 
+const Posts = ({ setCurrentId }) => {
+  const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
+
+ 
+    )
+  );
+};
 
 export default Posts;
