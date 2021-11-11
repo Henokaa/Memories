62d2f9a7debbd05c6eb8101b4298cdedcea00bde
Henokaa <69524541+Henokaa@users.noreply.github.com>
2021-11-11 09:13:09 +0300

    post cards

---
+++ my-react-app/src/components/Posts/Post/Post.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core/';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import moment from 'moment';
+import { useDispatch } from 'react-redux';
+
+import { likePost, deletePost } from '../../../actions/posts';
+import useStyles from './styles';
+
+const Post = ({ post, setCurrentId }) => {
+  const dispatch = useDispatch();
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      
+    </Card>
+  );
+};
+
+export default Post;
