316af23f2380d2616d2278398441c758cb687213
Henokaa <69524541+Henokaa@users.noreply.github.com>
2021-11-14 00:47:23 +0300

    grid style

--- my-react-app/src/components/Posts/Post/styles.js
+++ my-react-app/src/components/Posts/Post/styles.js
@@ -27,5 +27,14 @@ export default makeStyles({
     left: '20px',
     color: 'white',
   },
+  overlay2: {
+    position: 'absolute',
+    top: '20px',
+    right: '20px',
+    color: 'white',
+  },
+  grid: {
+    display: 'flex',
+  },
   
 });
